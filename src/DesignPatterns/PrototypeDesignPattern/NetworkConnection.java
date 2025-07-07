5c75c1d7554c3db2ce3b4716c73a973737e9cc36
Ashutosh <ashu.ksing1018@gmail.com>
2025-07-08 01:07:47 +0530

    prototype

---
+++ src/DesignPatterns/PrototypeDesignPattern/NetworkConnection.java
@@ -0,0 +1,47 @@
+package DesignPatterns.PrototypeDesignPattern;
+
+public class NetworkConnection implements Cloneable{
+
+    private String ip;
+    private String data;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void loadData() throws InterruptedException {
+        this.data = "This is a very important data";
+        Thread.sleep(5000);
+    }
+
+    @Override
+    public String toString() {
+        return "NetworkConnection{" +
+                "ip='" + ip + '\'' +
+                ", data='" + data + '\'' +
+                '}';
+    }
+
+    @Override
+    public NetworkConnection clone() {
+        try {
+            NetworkConnection clone = (NetworkConnection) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
