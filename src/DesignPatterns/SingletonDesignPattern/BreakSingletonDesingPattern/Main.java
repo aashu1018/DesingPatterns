b8cefd9fd2c5b69c090a9fc6073c4de5a2a47098
Ashutosh <ashu.ksing1018@gmail.com>
2025-10-30 13:47:43 +0530

    comments

--- src/DesignPatterns/SingletonDesignPattern/BreakSingletonDesingPattern/Main.java
+++ src/DesignPatterns/SingletonDesignPattern/BreakSingletonDesingPattern/Main.java
@@ -11,7 +11,10 @@ public class Main {
 
         //Reflection api
         Constructor<SingletonClass> constructor = SingletonClass.class.getDeclaredConstructor();
-        constructor.setAccessible(true);
+        constructor.setAccessible(true); //Actually invokes the constructor and creates a new object.
+        //It does not call getInstance() — it directly calls the constructor at the JVM level
+        //we are bypassing getInstance() using Java reflection - that's the whole purpose right?
+
         SingletonClass singletonClass1 = constructor.newInstance();
         System.out.println(singletonClass1.hashCode());
 
